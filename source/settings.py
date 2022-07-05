2310af4f21ae83f520d73bf2dd8741bfe27c5879
mohan77java <mohanbhole7@gmail.com>
2022-07-05 17:37:33 +0530

    Revert "Update settings.py"
    This reverts commit b345a66d263a85d42dc0dea885a868358e488721.

--- source/settings.py
+++ source/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f+@qe(ei%n#@h^*0$54vl#a9ukrgfa70wa3ed!47g7k#_c+76j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['online-digonstic-lab-reporting.herokuapp.com']
 
